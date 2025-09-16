e1609239e8ac4a3f37bd9ba2c6bf7b55429a32fa
David Levy <dlevy.engineer@gmail.com>
2025-09-16 11:25:08 -0700

    Adjusted distortion computation to MSE rather than L2 norm.

--- src/haag_vq/metrics/distortion.py
+++ src/haag_vq/metrics/distortion.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def compute_distortion(X_original, X_compressed_codes, pq):
-    X_reconstructed = pq.decompress(X_compressed_codes)
+def compute_distortion(X_original, X_compressed_codes, model):
+    X_reconstructed = model.decompress(X_compressed_codes)
     diffs = X_original - X_reconstructed
-    return np.mean(np.linalg.norm(diffs, axis=1))
+    return np.mean(np.sum(diffs ** 2, axis=1))
