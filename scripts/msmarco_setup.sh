c141c21c80855bea2566127e4a2d14e160b775bd
Nabrissa Khan <82301901+nabrissakhan@users.noreply.github.com>
2025-10-12 18:47:31 -0600

    Update echo message in mmarco_setup.sh

--- scripts/msmarco_setup.sh
+++ scripts/msmarco_setup.sh
@@ -7,5 +7,5 @@ DATA=~/scratch/msmarco
 mkdir -p "$REPO/data/msmarco"
 ln -sf "$DATA/collection.tsv" "$REPO/data/msmarco/collection.tsv"
 
-echo "✅ Symlink created:"
+echo "Symlink created:"
 echo "$REPO/data/msmarco/collection.tsv -> $DATA/collection.tsv"
